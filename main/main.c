40024a9dc4ea1ee1dec53c28f8d62fd544621644
Venanciofaf <venanciofaf@al.insper.edu.br>
2025-04-14 11:02:52 -0300

    Infra Feita

--- main/main.c
+++ main/main.c
@@ -12,125 +12,124 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
-const uint BTN_1_OLED = 28;
-const uint BTN_2_OLED = 26;
-const uint BTN_3_OLED = 27;
+const int BTN_RM = 15;
+const int BTN_RM = 14;
+const int BTN_SCROLL = 13;
+const int BTN_INVENTORY = 12;
+const int BTN_JUMP = 11;
+const int BTN_RUN = 10;
 
-const uint LED_1_OLED = 20;
-const uint LED_2_OLED = 21;
-const uint LED_3_OLED = 22;
 
-void oled1_btn_led_init(void) {
-    gpio_init(LED_1_OLED);
-    gpio_set_dir(LED_1_OLED, GPIO_OUT);
+QueueHandle_t xQueueComandos;
 
-    gpio_init(LED_2_OLED);
-    gpio_set_dir(LED_2_OLED, GPIO_OUT);
 
-    gpio_init(LED_3_OLED);
-    gpio_set_dir(LED_3_OLED, GPIO_OUT);
 
-    gpio_init(BTN_1_OLED);
-    gpio_set_dir(BTN_1_OLED, GPIO_IN);
-    gpio_pull_up(BTN_1_OLED);
+typedef struct {
+    uint8_t tipo;   // 0 = joystick1
+    uint8_t tecla;  // 0 = W, 1 = A, 2 = S, 3 = D
+    uint8_t estado; // 1 = pressionado, 0 = solto
+} evento_t;
 
-    gpio_init(BTN_2_OLED);
-    gpio_set_dir(BTN_2_OLED, GPIO_IN);
-    gpio_pull_up(BTN_2_OLED);
 
-    gpio_init(BTN_3_OLED);
-    gpio_set_dir(BTN_3_OLED, GPIO_IN);
-    gpio_pull_up(BTN_3_OLED);
-}
 
-void oled1_demo_1(void *p) {
-    printf("Inicializando Driver\n");
-    ssd1306_init();
+void move_x_task(void *p) {
 
-    printf("Inicializando GLX\n");
-    ssd1306_t disp;
-    gfx_init(&disp, 128, 32);
 
-    printf("Inicializando btn and LEDs\n");
-    oled1_btn_led_init();
 
-    char cnt = 15;
-    while (1) {
+    while (true) {
 
-        if (gpio_get(BTN_1_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_1_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 1 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_2_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_2_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 2 - ON");
-            gfx_show(&disp);
-        } else if (gpio_get(BTN_3_OLED) == 0) {
-            cnt = 15;
-            gpio_put(LED_3_OLED, 0);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "LED 3 - ON");
-            gfx_show(&disp);
-        } else {
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando BTN_TASK");
 
-            gpio_put(LED_1_OLED, 1);
-            gpio_put(LED_2_OLED, 1);
-            gpio_put(LED_3_OLED, 1);
-            gfx_clear_buffer(&disp);
-            gfx_draw_string(&disp, 0, 0, 1, "PRESSIONE ALGUM");
-            gfx_draw_string(&disp, 0, 10, 1, "BOTAO");
-            gfx_draw_line(&disp, 15, 27, cnt,
-                          27);
-            vTaskDelay(pdMS_TO_TICKS(50));
-            if (++cnt == 112)
-                cnt = 15;
-
-            gfx_show(&disp);
-        }
     }
 }
 
-void oled1_demo_2(void *p) {
-    printf("Inicializando Driver\n");
-    ssd1306_init();
 
-    printf("Inicializando GLX\n");
-    ssd1306_t disp;
-    gfx_init(&disp, 128, 32);
+void move_y_task(void *p) {
 
-    printf("Inicializando btn and LEDs\n");
-    oled1_btn_led_init();
 
-    char cnt = 15;
-    while (1) {
 
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 1, "Mandioca");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
+    while (true) {
 
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 2, "Batata");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando BTN_TASK");
 
-        gfx_clear_buffer(&disp);
-        gfx_draw_string(&disp, 0, 0, 4, "Inhame");
-        gfx_show(&disp);
-        vTaskDelay(pdMS_TO_TICKS(150));
     }
 }
+
+void look_x_task(void *p) {
+
+
+
+    while (true) {
+
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando BTN_TASK");
+
+    }
+}
+
+void look_y_task(void *p) {
+
+
+
+    while (true) {
+
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando BTN_TASK");
+
+    }
+}
+
+
+
+void btn_task(void *p) {
+
+
+
+    while (true) {
+
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando BTN_TASK");
+
+    }
+}
+
+
+void uart_task(void *p) {
+
+
+
+    while (true) {
+
+        
+        vTaskDelay(pdMS_TO_TICKS(100));
+        printf("Rodando UART_TASK");
+    }
+}
+
 
 int main() {
     stdio_init_all();
 
-    xTaskCreate(oled1_demo_2, "Demo 2", 4095, NULL, 1, NULL);
+
+    xQueue = xQueueCreate(32 , sizeof(evento_t));
+
+    
+    xTaskCreate(move_x_task, "Task de Mover em X", 4095, NULL, 1, NULL);
+    xTaskCreate(move_y_task, "Task de Mover em Y", 4095, NULL, 1, NULL);
+    xTaskCreate(look_x_task, "Task de Olhar em X", 4095, NULL, 1, NULL);
+    xTaskCreate(look_y_task, "Task de Olhar em Y", 4095, NULL, 1, NULL);
+    xTaskCreate(uart_task, "Task do UART", 4095, NULL, 1, NULL);     
+    xTaskCreate(btn_task, "Task de Botões", 4095, NULL, 1, NULL);
 
     vTaskStartScheduler();
+
 
     while (true)
         ;
